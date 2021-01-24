3666655e46085d69fedb28ba90ea6142be8b721a
Thomas Wenzlaff <bit@wenzlaff.de>
2021-01-24 19:08:04 +0100

    get24Wortliste mit validate erg.

--- src/main/java/de/wenzlaff/beispiel/Start.java
+++ src/main/java/de/wenzlaff/beispiel/Start.java
@@ -5,20 +5,34 @@ import java.security.SecureRandom;
 import java.security.Security;
 
 import io.github.novacrypto.bip39.MnemonicGenerator;
+import io.github.novacrypto.bip39.MnemonicValidator;
 import io.github.novacrypto.bip39.Words;
+import io.github.novacrypto.bip39.Validation.InvalidChecksumException;
+import io.github.novacrypto.bip39.Validation.InvalidWordCountException;
+import io.github.novacrypto.bip39.Validation.UnexpectedWhiteSpaceException;
+import io.github.novacrypto.bip39.Validation.WordNotFoundException;
 import io.github.novacrypto.bip39.wordlists.English;
 
 /**
- * Ausgabe der 24 BIP39 Wörter in Englisch und er im System verwendeten Security
- * Provider.
+ * Ausgabe der 24 BIP39 Wörter in Englisch und der im System verwendeten
+ * Security Provider.
  * 
  * @author Thomas Wenzlaff
  */
 public class Start {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 
 		getSecurityProviders();
+
+		String wortliste = get24Wortliste();
+
+		System.out.println("24 BIP39 Wörter in englisch:");
+		System.out.println(wortliste);
+
+	}
+
+	private static String get24Wortliste() throws InvalidChecksumException, InvalidWordCountException, WordNotFoundException, UnexpectedWhiteSpaceException {
 
 		SecureRandom secureRan = new SecureRandom();
 		System.out.println("Verwende Provider: " + secureRan.getProvider());
@@ -30,8 +44,9 @@ public class Start {
 		StringBuilder sb = new StringBuilder();
 		new MnemonicGenerator(English.INSTANCE).createMnemonic(entropy, sb::append);
 
-		System.out.println("24 BIP39 Wörter in englisch:");
-		System.out.println(sb.toString());
+		MnemonicValidator.ofWordList(English.INSTANCE).validate(sb.toString());
+
+		return sb.toString();
 	}
 
 	private static Provider[] getSecurityProviders() {
