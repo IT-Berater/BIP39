b3b4bfc392d82e878f88bf7f260a98e78164fec5
Thomas Wenzlaff <bit@wenzlaff.de>
2021-01-24 17:11:49 +0100

    Beispiel

---
+++ src/main/java/de/wenzlaff/beispiel/Start.java
@@ -0,0 +1,45 @@
+package de.wenzlaff.beispiel;
+
+import java.security.Provider;
+import java.security.SecureRandom;
+import java.security.Security;
+
+import io.github.novacrypto.bip39.MnemonicGenerator;
+import io.github.novacrypto.bip39.Words;
+import io.github.novacrypto.bip39.wordlists.English;
+
+/**
+ * Ausgabe der 24 BIP39 Wörter in Englisch und er im System verwendeten Security
+ * Provider.
+ * 
+ * @author Thomas Wenzlaff
+ */
+public class Start {
+
+	public static void main(String[] args) {
+
+		getSecurityProviders();
+
+		SecureRandom secureRan = new SecureRandom();
+		System.out.println("Verwende Provider: " + secureRan.getProvider());
+
+		byte[] entropy = new byte[Words.TWENTY_FOUR.byteLength()];
+
+		secureRan.nextBytes(entropy);
+
+		StringBuilder sb = new StringBuilder();
+		new MnemonicGenerator(English.INSTANCE).createMnemonic(entropy, sb::append);
+
+		System.out.println("24 BIP39 Wörter in englisch:");
+		System.out.println(sb.toString());
+	}
+
+	private static Provider[] getSecurityProviders() {
+		System.out.println("Alle im System bekannten Security Provider:");
+		Provider[] provider = Security.getProviders();
+		for (int i = 0; i < provider.length; i++) {
+			System.out.println(provider[i]);
+		}
+		return provider;
+	}
+}
