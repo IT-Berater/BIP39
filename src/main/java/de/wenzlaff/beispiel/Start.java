8389573f4abcce337b301fe9f244c6548a78cde8
Thomas Wenzlaff <bit@wenzlaff.de>
2021-01-27 20:55:13 +0100

    Dazu BPMN erstellt

--- src/main/java/de/wenzlaff/beispiel/Start.java
+++ src/main/java/de/wenzlaff/beispiel/Start.java
@@ -20,7 +20,8 @@ import io.github.novacrypto.bip39.wordlists.English;
 
 /**
  * Ausgabe der 24 BIP39 Wörter in englisch und der im System verwendeten
- * Security Provider.
+ * Security Provider. Beispiel für die Generierung der privaten und öffentlichen
+ * Key sowie die Adressen.
  * 
  * Siehe auch online:
  * 
@@ -28,56 +29,73 @@ import io.github.novacrypto.bip39.wordlists.English;
  * 
  * http://bip39.de/
  * 
+ * http://www.wenzlaff.info
+ * 
  * @author Thomas Wenzlaff
  */
 public class Start {
 
+	/**
+	 * Optionales Passwort.
+	 */
+	private static final String KEIN_BIP39_PASSWORT = "";
+	/**
+	 * Der BIP32 abgeleitete Pfad für BTC (Bitcoine)
+	 */
 	private static final String BIP32_ABGELEITETER_PFAD = "m/0'/0'";
 
+	/**
+	 * Testcode.
+	 * 
+	 * @param args
+	 * @throws Exception
+	 */
 	public static void main(String[] args) throws Exception {
 
-		System.out.println("\nErzeuge zufällige Wortliste:\n");
+		System.out.println("Starte das erzeuge zufällige Wortliste mit 24 Wörtern");
 		String wortliste = get24Wortliste();
 
 		System.out.println("\n24 BIP39 Mnemonic Wörter in englisch:");
 		System.err.println(wortliste);
 
 		SeedCalculator seedCal = new SeedCalculator();
-		CharSequence bip39Passphrase = "sicher"; // optional ohne gleich ""
+		CharSequence bip39Passphrase = KEIN_BIP39_PASSWORT;
 
-		System.out.println("\nBIP39 optionale Passphrase: " + bip39Passphrase);
+		System.out.println("\nBIP39 optionale Passphrase (Achtung, nicht für alle Geräte (ledgers) erlauben eine Passphrase");
+		System.out.println("Verwende Passwort für BIB39 Seed:");
+		System.err.println(bip39Passphrase);
 
-		System.out.println("Berechne aus BIP39 Wortliste und optionanle Passphrase den Seed:");
+		System.out.println("Berechne aus BIP39 Wortliste und optionalen Passphrase den Seed");
 		byte[] calculateSeed = seedCal.calculateSeed(wortliste, bip39Passphrase.toString());
 
-		System.err.println("\nBIP39 Seed in Hex:\n" + encodeBytesToHex(calculateSeed));
+		System.out.println("\nBIP39 Seed in Hex:");
+		System.err.println(encodeBytesToHex(calculateSeed));
 
-		System.out.println("\nBerechne mit dem Seed und den Coin (BTC - Bitcoin) den BIP 32 erweiterten privaten Schlüssel:");
+		System.out.println("\nBerechne mit dem Seed und den Coin (BTC - Bitcoin) den BIP 32 privaten Schlüssel");
 
 		ExtendedPrivateKey privateKey = ExtendedPrivateKey.fromSeed(calculateSeed, Bitcoin.MAIN_NET);
 		String extendedKey = privateKey.extendedBase58();
 
-		System.err.println("\nBIP32 private Key für Bitcoin(BTC) (BIP32 Root Key, BIP32 Wurzelschlüssel):\n" + extendedKey);
+		System.out.println("\nBIP32 private Key für Bitcoin(BTC) (BIP32 Root Key, BIP32 Wurzelschlüssel):");
+		System.err.println(extendedKey);
 
 		// BIP32 Erweiterter Privatschlüssel
-
 		System.out.println(
-				"\nLeite vom privaten Key und BIP32-abgeleiteten Pfad (" + BIP32_ABGELEITETER_PFAD + ") (Bitcoin Core) den erweiterten privaten Schlüssel ab:");
-
+				"\nLeite vom privaten Key und BIP32-abgeleiteten Pfad (" + BIP32_ABGELEITETER_PFAD + ") (Bitcoin Core) den erweiterten privaten Schlüssel ab");
 		ExtendedPrivateKey abgeleiteterPrivateKey = privateKey.derive(BIP32_ABGELEITETER_PFAD);
-
-		System.out.println("\nBIP32 Erweiterter Privatschlüssel:\n" + abgeleiteterPrivateKey.extendedBase58());
+		System.out.println("\nBIP32 Erweiterter Privatschlüssel:");
+		System.err.println(abgeleiteterPrivateKey.extendedBase58());
 
 		// BIP32 Erweiterter öffentlicher Schlüssel
-
 		ExtendedPublicKey abgeleiteterPublicKey = abgeleiteterPrivateKey.neuter();
-
-		System.out.println("\nBIP32 Erweiterter öffentlicher Schlüssel:\n" + abgeleiteterPublicKey.extendedBase58());
+		System.out.println("\nBIP32 Erweiterter öffentlicher Schlüssel:");
+		System.out.println(abgeleiteterPublicKey.extendedBase58());
 
 		// Abgeleitete Adressen
 		// Beachten Sie, dass diese Adressen vom erweiterten BIP32-Schlüssel abgeleitet
 		// sind, und nun mal drei Adressen ausgeben
 
+		System.out.println("\nAdressen:");
 		String addressMethod = privateKey.derive("m/0'/0'/0").neuter().p2pkhAddress();
 		System.out.println("0. Adresse: " + addressMethod);
 		addressMethod = privateKey.derive("m/0'/0'/1").neuter().p2pkhAddress();
