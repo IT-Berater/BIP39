8f8d2f59d0d9bcce5a489ebc639cecc0a01c83db
Thomas Wenzlaff <bit@wenzlaff.de>
2021-01-27 21:35:10 +0100

    Start script

---
+++ crypto-libs-distro/start-java.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Thomas Wenzlaff
+# siehe www.wenzlaff.info
+# Beispielcode für Wortlisten und Key generierung
+
+java -cp BIP39-2019.01.27.jar:ToRuntime-2019.01.27.jar:BIP32derivation-2019.01.27.jar:BIP32-2019.01.27.jar:SHA256-2019.01.27.jar:bcprov-jdk15on-1.64.jar:core-1.58.0.0.jar:Base58-2019.01.27.jar:Hash160-2019.01.27.jar de.wenzlaff.beispiel.Start
